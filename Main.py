0f69bc8fc3426800086be66a1df6c866facf0497
CoderCris <crisamer.ortega@gmail.com>
2018-12-23 16:59:32 +0000

    add basic files

---
+++ Main.py
@@ -0,0 +1,33 @@
+from random import randint
+from sys import argv
+
+script, polarity = argv
+
+enterprises = open(enterprises)
+relationships = open(relationships)
+
+
+
+def main ()
+    loadFiles()
+    print define()
+
+
+def define():
+
+    return enterprise() + relationship(roll, polarity) + enterprise(roll)
+
+def enterprise():
+
+    while(enterprises.readLine() != null)
+        #¡¡¡lo deje aqui por pereza, pero la idea es que esto cuente el numero delineas
+        #escritas para poder hacer la tirada random, no es elegante, pero es funcional
+
+
+def relationship():
+
+
+
+def int roll ():
+
+    return randint(0, faces-1)
